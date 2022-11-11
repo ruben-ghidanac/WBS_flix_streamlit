38b048e2588d0a20a738c10b64c98f01ff9d27e7
Ruben Ghidanac <57212930+ruben-ghidanac@users.noreply.github.com>
2022-11-11 13:53:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from src.functions import *
 
+"""## Movie Challenge:
+Your task: create a function that takes the users userId, and a number (n) and outputs the n most recommended movies based on the cosine similarity of other users.
+"""
+
 UserID = st.number_input("Please select your UserID",min_value=1, step=1)
 Top_n_movies = st.number_input("Please select a number of Movies which you want to be recommended for you:",min_value=1, step=1)
 
