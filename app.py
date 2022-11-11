1f9365e1b12b3b82a8cf6cd047ace32942ad0510
Ruben Ghidanac <57212930+ruben-ghidanac@users.noreply.github.com>
2022-11-11 12:24:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from src.functions import *
 
-UserID = st.number_input("UserID",min_value=1, step=1)
-Top_n_movies = st.number_input("Top_n_movies",min_value=1, step=1)
+UserID = st.number_input("Please select your UserID",min_value=1, step=1)
+Top_n_movies = st.number_input("Please select a number of Movies which you want to be recommended for you:",min_value=1, step=1)
 
 st.write("We have", Top_n_movies, "movie/s recomandation for user:", UserID, recommendations_for_specific_user_id(UserID, Top_n_movies))
