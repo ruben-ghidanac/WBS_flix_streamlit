35e3d41924b7535d6467fc1f09dffd92c2ebbce7
Ruben Ghidanac <57212930+ruben-ghidanac@users.noreply.github.com>
2022-11-11 14:09:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ UserID = st.number_input("Please select your UserID",min_value=1, step=1)
 Top_n_movies = st.number_input("Please select a number of Movies which you want to be recommended for you:",min_value=1, step=1)
 
 st.write("We have", Top_n_movies, "movie/s recomandation for user:", UserID, recommendations_for_specific_user_id(UserID, Top_n_movies))
+
+
+"""
+And if you are curious about what movies a particular user has seen already, you can see them below:
+"""
+UserID2 = st.number_input("Please select your UserID",min_value=1, step=1)
+st.write("The user", UserID2, "has seen already this/these movie/s:", show_seen_movies_for_specific_user_id(UserID2))
