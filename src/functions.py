a989a49eab33f407da3fa2cea19ab8f0f3bdce6b
Ruben Ghidanac <57212930+ruben-ghidanac@users.noreply.github.com>
2022-11-11 13:45:06 +0100

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import MinMaxScaler
 
 # Read the data
 
@@ -24,8 +25,10 @@ users_items = pd.pivot_table(data=ratings, values='rating', index='movieId', col
 Your task: create a function that takes the users userId, and a number (n) and outputs the n most recommended movies based on the cosine similarity of other users.
 """
 
-def recommendations_for_specific_user_id(user_id, top_n_movies):
+# your code here
 
+def recommendations_for_specific_user_id(user_id, top_n_movies):
+  
   # replace NaNs with zeros, the cosine similarity can't be computed with NaN's
   users_items.fillna(0, inplace=True)
 
@@ -43,13 +46,23 @@ def recommendations_for_specific_user_id(user_id, top_n_movies):
   not_seen_movies = users_items.loc[users_items.index!=user_id, users_items.loc[user_id,:]==0]
   not_seen_movies.T
 
-  # dot product between the not-seen-movies and the weights
-  weighted_averages = pd.DataFrame(not_seen_movies.T.dot(weights), columns=["predicted_rating"])
+  # dot product between the not-visited-restaurants and the weights
+  weighted_averages = pd.DataFrame(not_seen_movies.T.dot(weights), columns=["Score recommendation"])
+  
+  # scale features
+  scaler = MinMaxScaler(feature_range=(0, 10), copy=False)
+  model=scaler.fit(weighted_averages)
+  scaled_data=model.transform(weighted_averages)
+  #weighted_averages = weighted_averages.round(2)
 
   # find the top 'n' movies from the rating predictions
   recommendations = weighted_averages.merge(movies, left_index=True, right_on="movieId")
-  top_n = recommendations.sort_values("predicted_rating", ascending=False).head(top_n_movies)
-  
+  recommendations = recommendations.drop(['movieId'], axis=1)
+  top_n = recommendations.sort_values("Score recommendation", ascending=False).head(top_n_movies)
+  top_n['Score recommendation'] = top_n['Score recommendation'].astype(int)
+  top_n.rename(columns = {'title':'Title', 'genres': 'Genres'}, inplace = True)
+  top_n = top_n.style.hide_index()
+
   return top_n
 
 
